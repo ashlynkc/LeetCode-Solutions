adec0488a7292b698d770fa86bfccd3ba0337b33
ashlynkc <61401033+ashlynkc@users.noreply.github.com>
2023-01-18 12:56:16 -0500

    Create jumpgame_greedy.java

---
+++ Java/jumpgame_greedy.java
@@ -0,0 +1,16 @@
+// Solution learned and followed by: https://www.youtube.com/watch?v=Yan0cv2cLy8
+// Java, Greedy algorithm solution
+// LeetCode No. 55
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int jumpIdx = nums.length - 1;
+        
+        for (int i = jumpIdx; i >= 0; i--) {
+            if (i + nums[jumpIdx] >= jumpIdx)
+                jumpIdx = i;
+        }
+
+        return jumpIdx == 0;
+    }
+}
